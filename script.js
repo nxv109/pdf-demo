064be7063fce1f11c4667d2a1afa8b2bfa6b084e
unknown <nxv109@gmail.com>
2021-08-28 23:38:14 +0700

    Updated script.js file

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
   window.onload = myApp;
 
+  const questionBoxEl = document.getElementById("question-box-id");
+
   function myApp() {
     for (var i = 0; i < 10; i++) {
-      $("#question-box-id").append(`
+      const questionBoxChildEl = document.createElement("div");
+
+      questionBoxChildEl.classList.add("question-box");
+      questionBoxChildEl.innerHTML = `
         <div class="question-box">
-          <div class="question-box__question">Dear developer, our mission is to serve all the best programming news you’ll ever need. Ready?</div>
+          <div class="question-box__question">Q.${
+            i + 1
+          } Dear developer, our mission is to serve all the best programming news you’ll ever need. Ready?</div>
           <div class="question-box__answers">
             <div class="question-box__answer">abc</div>
             <div class="question-box__answer">abc</div>
@@ -13,7 +20,8 @@ $(document).ready(function () {
             <div class="question-box__answer">abc</div>
           </div>
         </div>
-    `);
+    `;
+      questionBoxEl.append(questionBoxChildEl);
     }
   }
 });
